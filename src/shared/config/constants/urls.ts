a6e6a00f90737c22479a3733817fb2dfc2c5c6d3
lazy_sl0th <anton.vlrch.ch@gmail.com>
2026-05-19 18:09:15 +0400

    correct config

--- src/shared/config/constants/urls.ts
+++ src/shared/config/constants/urls.ts
@@ -2,6 +2,10 @@ const { NODE_ENV, API_URL, WS_URL } = process.env
 
 const isProd = NODE_ENV === 'production'
 
+console.log(NODE_ENV)
+console.log(API_URL)
+console.log(WS_URL)
+
 export const BASE_API = isProd && API_URL ? API_URL : 'http://localhost:3001'
 
 export const BASE_WSAPI = isProd && WS_URL ? WS_URL : 'http://localhost:3001'
