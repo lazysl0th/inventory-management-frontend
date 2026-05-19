af2f97f6d85d73682aea2ab5d147098d7dae577f
lazy_sl0th <anton.vlrch.ch@gmail.com>
2026-05-19 18:17:15 +0400

    correct config

--- src/shared/config/constants/urls.ts
+++ src/shared/config/constants/urls.ts
@@ -1,18 +1,10 @@
 const { NODE_ENV, API_URL, WS_URL } = process.env
 
-const isProd = NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production'
 
-console.log(process.env)
-console.log(process.env.NODE_ENV)
-console.log(process.env.API_URL)
-console.log(process.env.WS_URL)
-console.log(NODE_ENV)
-console.log(API_URL)
-console.log(WS_URL)
+export const BASE_API = isProd && process.env.API_URL ? process.env.API_URL : 'http://localhost:3001'
 
-export const BASE_API = isProd && API_URL ? API_URL : 'http://localhost:3001'
-
-export const BASE_WSAPI = isProd && WS_URL ? WS_URL : 'http://localhost:3001'
+export const BASE_WSAPI = isProd && process.env.WS_URL ? process.env.WS_URL : 'http://localhost:3001'
 
 export const GOOGLE_AUTH_URL = `${BASE_API}/signin/google`
 
